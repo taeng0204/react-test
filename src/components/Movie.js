6994b211b3992154e2b2f89bdc1b4b10a031406e
taeng0204 <taeng02@icloud.com>
2022-09-20 12:47:14 +0900

    test

---
+++ src/components/Movie.js
@@ -0,0 +1,20 @@
+import {
+  Link
+} from "react-router-dom";
+
+function Movie({id, coverImg, title, summary, genres}) {
+  return <div>
+    <div>
+      <img src={coverImg} alt={title}/>
+      <h2><Link to={`movie/${id}`}>{title}</Link></h2>
+      <p>{summary.length > 235 ? `${summary.slice(0, 235)} ...` : summary}</p>
+      <ul>
+        {genres.map(g => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+</div>;
+}
+
+export default Movie;
